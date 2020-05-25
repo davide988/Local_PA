b63ce84bafc34c269c0c876a30d0768c115e88b1
davide988 <58575400+davide988@users.noreply.github.com>
2020-05-25 18:51:23 +0200

    create_page

---
+++ src/app/create-party/create-party.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-party',
+  templateUrl: './create-party.page.html',
+  styleUrls: ['./create-party.page.scss'],
+})
+export class CreatePartyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
