03fe09c3b15d137503b738b8f1b3450eb488e85e
Andrea <andripulc@hotmail.it>
2020-05-26 17:46:36 +0200

    commit 3

--- src/app/create-party/create-party.page.ts
+++ src/app/create-party/create-party.page.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import {Routes, RouterModule, Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth/auth.service';
+import {AlertController, LoadingController, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-create-party',
   templateUrl: './create-party.page.html',
   styleUrls: ['./create-party.page.scss'],
 })
-export class CreatePartyPage implements OnInit {
+export class CreatePartyPage  {
+
 
   constructor(
-      private router: Router
+      private router: Router,
+  private PartyService: AuthService,
+  private alertCtrl: AlertController,
+  private toastCtrl: ToastController,
+  private loadingCtrl: LoadingController
 
   ) {}
+
+  form = new FormGroup({
+    firstname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    lastname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
 
   ngOnInit() {
   }
